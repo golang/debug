9ab610643a4bc0dcbe857e9ae8f60fbc7274fabe
Nigel Tao <nigeltao@golang.org>
2014-02-14 13:24:21 +1100

    ogle/probe: add overflow checks for validRead and validWrite.
    LGTM=r
    R=r
    https://golang.org/cl/63010043

--- probe/probe.go
+++ probe/probe.go
@@ -22,7 +22,8 @@ func heapEnd() uintptr
 
 // validRead reports whether a read of the specified size can be done at address p.
 func validRead(p uintptr, size int) bool {
-	if size < 0 {
+	// Check for negative size and for (p + size) overflow.
+	if size < 0 || uint64(^uintptr(0)-p) < uint64(size) {
 		return false
 	}
 	// The read must be in a single contiguous valid region.
@@ -40,7 +41,8 @@ func validRead(p uintptr, size int) bool {
 
 // validWrite reports whether a write of the specified size can be done at address p.
 func validWrite(p uintptr, size int) bool {
-	if size < 0 {
+	// Check for negative size and for (p + size) overflow.
+	if size < 0 || uint64(^uintptr(0)-p) < uint64(size) {
 		return false
 	}
 	// The write must be in a single contiguous valid region.
