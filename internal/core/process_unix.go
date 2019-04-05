19561fee47cf8cd0400d1b094c5898002f97cf90
Brad Fitzpatrick <bradfitz@golang.org>
2019-04-05 15:25:35 +0000

    internal/core: fix build on Solaris
    Solaris has unix.Mmap but not syscall.Mmap.
    
    Change-Id: Ia48b1db21d48caa835e43ea4f6616c8c3d06e089
    Reviewed-on: https://go-review.googlesource.com/c/debug/+/170786
    Run-TryBot: Brad Fitzpatrick <bradfitz@golang.org>
    TryBot-Result: Gobot Gobot <gobot@golang.org>
    Reviewed-by: Tobias Klauser <tobias.klauser@gmail.com>

--- internal/core/process_unix.go
+++ internal/core/process_unix.go
@@ -6,10 +6,14 @@
 
 package core
 
-import "syscall"
+import (
+	"syscall"
+
+	"golang.org/x/sys/unix"
+)
 
 func init() {
 	mapFile = func(fd int, offset int64, length int) (data []byte, err error) {
-		return syscall.Mmap(fd, offset, length, syscall.PROT_READ, syscall.MAP_SHARED)
+		return unix.Mmap(fd, offset, length, syscall.PROT_READ, syscall.MAP_SHARED)
 	}
 }
